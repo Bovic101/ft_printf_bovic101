af53b5666256a2a47c2d2b2cf57b444421052beb
vodebunm <vodebunm@student.42heilbronn.de>
2023-12-06 03:21:36 +0100

    First update of my custom prinft functiom

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 03:07:54 by vodebunm          #+#    #+#             */
+/*   Updated: 2023/12/06 01:46:49 by vodebunm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include "libft.h"
+
+int	ft_prinft(const char *s, ...);
+
+#endif
