31511114f2acf188d9d47043fa498c6a4bfd437a
vodebunm <vodebunm@student.42heilbronn.de>
2024-02-04 06:32:46 +0100

    ft_printf source file addition

--- ft_printf/ft_str_printf.c
+++ ft_printf/ft_str_printf.c
@@ -1,20 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_str_printf.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/04 05:56:15 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/02/04 06:01:15 by vodebunm         ###   ########.fr       */
+/*   Created: 2024/02/04 05:22:04 by vodebunm          #+#    #+#             */
+/*   Updated: 2024/02/04 05:46:07 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *s, ...)
+// Function to print a string using the variadic function
+int	ft_str_printf(va_list list)
 {
-	va_list	list;
-	int		i;
-	int		len;
+	char	*s;
+
+	s = va_arg(list, char *);
+	if (!s)
+	{
+		ft_putstr_fd("(nil)", 1);
+		return (5);
+	}
+	ft_putstr_fd(s, 1);
+	return (ft_strlen(s));
 }
