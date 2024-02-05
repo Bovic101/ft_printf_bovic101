30c457d562e202865eab0349f93412566485c5ec
Bovic101 <Victorbolanle72@gmail.com>
2024-02-05 21:42:32 +0100

    integer and decimal _print function update

--- ft_printf/ft_conversion_specifier.c
+++ ft_printf/ft_conversion_specifier.c
@@ -1,25 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_conversion_specifier.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/04 03:07:54 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/02/05 21:22:10 by vodebunm         ###   ########.fr       */
+/*   Created: 2024/02/05 21:01:43 by vodebunm          #+#    #+#             */
+/*   Updated: 2024/02/05 21:06:45 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include	"ft_printf.h"
 
-# include <stdarg.h>
-# include "libft.h"
-
-int	ft_prinft(const char *s, ...);
-int	ft_str_printf(va_list list);
-int	ft_hexaconvert_printf(va_list list);
-int	ft_hexaconvert_up_printf(va_list list);
-int	ft_int_dec_number_print(long long int n);
-
-#endif
+int	ft_conversion_specifier(const char c, va_list list, int *i)
+{
+	
+}
--- ft_printf/ft_int_dec_number_print.c
+++ ft_printf/ft_int_dec_number_print.c
@@ -1,25 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_int_dec_number_print.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/04 03:07:54 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/02/05 21:22:10 by vodebunm         ###   ########.fr       */
+/*   Created: 2024/02/05 21:19:29 by vodebunm          #+#    #+#             */
+/*   Updated: 2024/02/05 21:41:25 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdarg.h>
-# include "libft.h"
+static void	ft_putnum(size_t n)
+{
+	if (n > 9)
+	{
+		ft_putnum(n / 10);
+		ft_putnum(n % 10);
+	}
+	if (n < 10)
+	{
+		ft_putchar_fd(n + 48, 1);
+	}
+}
 
-int	ft_prinft(const char *s, ...);
-int	ft_str_printf(va_list list);
-int	ft_hexaconvert_printf(va_list list);
-int	ft_hexaconvert_up_printf(va_list list);
-int	ft_int_dec_number_print(long long int n);
+int	ft_int_dec_number_print(long long int n)
+{
+	int	count;
 
-#endif
+	count = 0;
+	if (n < 0)
+	{
+		ft_putchar_fd('-', 1);
+		n = -n;
+		count++;
+	}
+	ft_putnum(n);
+	count += ft_num_length(n, 10);
+	return (count);
+}
